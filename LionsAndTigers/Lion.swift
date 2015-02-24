4204ffc61f4564781556e75434c51a7000c746ae
Daniel <daniello130@o2.pl>
2015-02-24 09:45:33 +0100

    first commit

---
+++ LionsAndTigers/Lion.swift
@@ -0,0 +1,36 @@
+//
+//  Lion.swift
+//  LionsAndTigers
+//
+//  Created by Daniel on 2/13/15.
+//  Copyright (c) 2015 Daniel Stradowski Codespining. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Lion {
+    var age = 0
+    var isAlphaMale = false
+    var image = UIImage(named:"")
+    var name = ""
+    var subspecies = ""
+    
+    func roar() {
+        println("Lion: Roar Roar")
+    }
+    
+    func changeToAlphaMale() {
+        self.isAlphaMale = true
+    }
+    
+    func randomFact() -> String {
+        var randomFact:String
+        if self.isAlphaMale {
+            randomFact = "Male lions are easy to recognize thanks to their distinctive manes. Males with darker manes are more likely to attract females."
+        } else {
+            randomFact = "Female Lionesses form the stable social unit and do not tolerate outside females."
+        }
+        return randomFact
+    }
+}
